6e7496e6ecaf27c9e65ed1fb303f29e2cd339e78
tarun kumar <tarunkhedar95@gmail.com>
2026-07-23 06:10:11 +0530

    files changes

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js';
 import session from 'express-session'
-import MongoStore from 'connect-mongo'
+import MongoStore = require('connect-mongo');
 import AuthRouter from './routes/AuthRoutes.js';
 import ThumbnailRouter from './routes/ThumbnailRoutes.js';
 import UserRouter from './routes/UserRoutes.js';
@@ -39,7 +39,7 @@ app.use(session({
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         path: '/'
     }, 
-    store: MongoStore.create({
+    store: (MongoStore as any).create({
         mongoUrl: process.env.MONGODB_URI as string,
         collectionName: 'sessions'
     })
